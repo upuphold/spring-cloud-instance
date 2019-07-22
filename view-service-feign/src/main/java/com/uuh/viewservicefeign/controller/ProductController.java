ce21c287e0bbb4b8ab4b76eaeaeedc58514aa3af
chengang <chengang@hiynn.com>
2019-07-22 18:05:20 +0800

    配置客户端

--- view-service-feign/src/main/java/com/uuh/viewservicefeign/controller/ProductController.java
+++ view-service-feign/src/main/java/com/uuh/viewservicefeign/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.uuh.viewservicefeign.controller;
 import com.uuh.viewservicefeign.entity.Product;
 import com.uuh.viewservicefeign.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +22,13 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 
+	@Value("${version}")
+	String version;
+
 	@RequestMapping("/products")
 	public Object products(Model model) {
 		List<Product> products = productService.products();
+		model.addAttribute("version", version);
 		model.addAttribute("ps", products);
 		return model;
 	}
